f8f0678e50b66def4eb3ba6dd9018d035325d3d5
bkfwebdev <bryant.feld@gmail.com>
2017-05-25 13:20:13 -0400

    git ignore added

--- config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    consumer_key:'QtPw8W3BSxrmaC0Airjb5h7uU',
-    consumer_secret:'7W7CNY0l5uPgxUKywLoWMCWdsN2CKny2r5rA9kpAafBLO7iaqG',
-    access_token:'822498423882518529-JktLyfwQOAhnSXKWfLX7BkcpGznAwt8',
-    access_token_secret: 'iOWPjhqGffL38saX5eSlcXrWymd5e5DTsaBYISolTwwV1',
-}
--- qbot.js
+++ qbot.js
@@ -5,6 +5,10 @@ var tweet = {};
 var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
+
+setInterval(bongBong, 60000);
+
+function bongBong (){
 function tweeted(err, data, response){
     if (err){console.log('something went wrong son!');
             console.log(err);
@@ -14,9 +18,11 @@ function tweeted(err, data, response){
         console.log('you got this my ninja');
     }
 }
-request('http://quotes.stormconsultancy.co.uk/random.json', function(error, response, data) {
+request('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', function(error, response, data) {
     var quoteObject = JSON.parse(data);
-    quoteString = quoteObject.quote 
-   tweet.status = quoteString
-   T.post('statuses/update',tweet , tweeted);
+    quoteString = quoteObject.quoteText; 
+    tweet.status = quoteString
+    T.post('statuses/update',tweet , tweeted);
+    console.log(quoteString)
 });
+}
